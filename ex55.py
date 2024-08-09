46c4389425c82bad630265f6a951758d33dab538
Millenna Mazaro <millenna424@gmail.com>
2024-08-09 15:13:46 -0300

    first commit

---
+++ ex55.py
@@ -0,0 +1,20 @@
+from random import randint
+
+# O computador escolhe um número aleatório entre 0 e 10
+
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpite = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpite += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... tente mais uma vez')
+print(f'Acertou com {palpite} tentativas. Parabéns!')
